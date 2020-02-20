a35b9a7bf390fff697eb94a52030adf9dd52b286
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-02-20 17:20:20 -0300

    some changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-
+const NunjucksWebpackPlugin = require("nunjucks-webpack-plugin");
 module.exports = {
   entry: {
     main: "./src/assets/js/script.js"
@@ -7,5 +7,15 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, `./public/assets/js`),
     filename: "[name].js"
-  }
+  },
+  plugins: [
+    new NunjucksWebpackPlugin({
+      templates: [
+        {
+          from: "./src/views/pages/home.njk",
+          to: "index.html"
+        }
+      ]
+    })
+  ]
 };
