ff915c354bff71c42bd318cc5970fbe3c1c04383
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-02-20 22:59:18 -0300

    some bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,31 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: {
     main: "./src/assets/js/script.js"
   },
   output: {
-    path: path.resolve(__dirname, `./public/assets/js`),
+    path: path.resolve(__dirname, `./public/`),
     filename: "[name].js"
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.njk$/,
+        use: [
+          {
+            loader: "simple-nunjucks-loader",
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Custom template using Handlebars",
+      template: "./src/views/pages/home.njk"
+    })
+  ]
 };
